e675fc56cd995b914c1b3ee5aba420beec8bdd70
Mariamadrame2001 <mariamadrame@mec.science>
2019-07-29 12:02:53 -0400

    script2.js

---
+++ script2.js
@@ -0,0 +1,25 @@
+function f(x)
+{
+    return 3 * x + 7;
+}
+//Displays the results of calling 
+
+function g(x)
+{
+    return 5*x + 5
+    console.log ("done")
+}
+Functions h(x)
+{
+    return 5*x*x -4*x + 2
+}
+
+Functions i 
+{
+    return (x,y)= x*x-y*y
+}
+
+console.log("\n");
+console.log(i(3,4));
+console.log(i(4,3));
+console.log(i(5,2));
