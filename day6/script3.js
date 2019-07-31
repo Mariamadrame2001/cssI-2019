b1e210aa664cafbf44be47d44005e7d90a684aeb
Mariamadrame2001 <mariamadrame@mec.science>
2019-07-31 10:32:23 -0400

    day6

---
+++ day6/script3.js
@@ -0,0 +1,14 @@
+function multiples (mult,m,n){
+
+var i;
+for (i = 0; i < multiples.length; i++){
+    multiple += multiple [i] + "<br>";
+}
+}
+for (i=0); i < n;i++){
+    mult[i]= m*(i+1);
+}
+return mult;
+}
+let myArray = [];
+console.logmultiples(myArray, 5, 10)
